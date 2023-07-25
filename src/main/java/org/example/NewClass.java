5063b137aa0e4ce4d4a96f9805aa9f53bace7593
Chaitanya Ramekar <ramekarc@gmail.com>
2023-07-25 17:50:33 +0530

    added explanation

--- src/main/java/org/example/NewClass.java
+++ src/main/java/org/example/NewClass.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NewClass extends JFrame {
+    //maze grid
     private int[][] maze =
             {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
                     {1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1},
@@ -28,7 +29,7 @@ public class NewClass extends JFrame {
 
     @Override
     public void paint(Graphics g) {
-        g.translate(50,50);
+        g.translate(50,50);//starting point of maze
         super.paint(g);
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[0].length; j++) {
@@ -36,10 +37,10 @@ public class NewClass extends JFrame {
                 if (maze[i][j] == 1) color = Color.BLACK;
                 else if (maze[i][j] == 9) color = Color.RED;
                 else color = Color.WHITE;
-            g.setColor(color);
-            g.fillRect(30 * j, 30 * i, 30, 30);
-            g.setColor(Color.RED);
-            g.drawRect(30 * j, 30 * i, 30, 30);
+            g.setColor(color);//setting inner squares color
+            g.fillRect(30 * j, 30 * i, 30, 30);//size of inner squares
+            g.setColor(Color.RED);//setting color of edges of inner squares
+            g.drawRect(30 * j, 30 * i, 30, 30);//drawing on the edges on inner squares
         }
     }
 }
