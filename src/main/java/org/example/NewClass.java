18df2c80945fc2713868556cd4a5646636a1a24d
Chaitanya Ramekar <ramekarc@gmail.com>
2023-07-26 14:04:28 +0530

    completion

--- src/main/java/org/example/NewClass.java
+++ src/main/java/org/example/NewClass.java
@@ -25,6 +25,8 @@ public class NewClass extends JFrame {
         setTitle("Maze");
         setSize(640, 720);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        DFS.pathsearch(maze,0,0,path);
+        //System.out.println(path); //to see if path is working or not
     }
 
     @Override
@@ -34,15 +36,23 @@ public class NewClass extends JFrame {
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[0].length; j++) {
                 Color color;
-                if (maze[i][j] == 1) color = Color.BLACK;
-                else if (maze[i][j] == 9) color = Color.RED;
+                if (maze[i][j] == 1) color = Color.DARK_GRAY;
+                else if (maze[i][j] == 9) color = Color.YELLOW;
                 else color = Color.WHITE;
             g.setColor(color);//setting inner squares color
             g.fillRect(30 * j, 30 * i, 30, 30);//size of innersquares
-            g.setColor(Color.RED);//setting color of edges of inner squares
+            g.setColor(Color.GREEN);//setting color of edges of inner squares
             g.drawRect(30 * j, 30 * i, 30, 30);//drawing on the edges on inner squares
         }
     }
+        for(int i=0;i<path.size();i+=2){
+            int pathx=path.get(i);
+            int pathy=path.get(i+1);
+            System.out.println("X coordinates"+pathx);
+            System.out.println("Y coordinates"+pathy);
+            g.setColor(Color.BLUE);
+            g.fillRect(30*pathx,30*pathy,30,30);
+        }
 }
     public static void main(String[] args) {
         NewClass view = new NewClass();
